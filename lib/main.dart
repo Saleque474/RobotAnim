dc3bdb3388702a93d954561d1f101fb0b78c56ad
Saleque474 <56844858+Saleque474@users.noreply.github.com>
2021-06-23 03:36:40 +0600

    Starting
    Here the Coding Start

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(RobotAnimApp());
+}
+
+class RobotAnimApp extends StatelessWidget {
+  const RobotAnimApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
